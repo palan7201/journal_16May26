29751d88a16c28bbd93d05081def6f851ae24a76
Palaniappan V <vallippanpalam2006@gmail.com>
2026-05-17 00:38:47 +0530

    space inconsistency fix

--- database.py
+++ database.py
@@ -4,10 +4,10 @@ import sqlite3
 import subprocess
 from datetime import datetime
 
-connection = sqlite3.connect ("journal.db")
-print ("Database connection successful")
+connection = sqlite3.connect("journal.db")
+print("Database connection successful")
 
-cursor = connection.cursor ()
+cursor = connection.cursor()
 
 schema = """CREATE TABLE IF NOT EXISTS entries (
               journal_id int primary key,
@@ -16,9 +16,9 @@ schema = """CREATE TABLE IF NOT EXISTS entries (
               created_at text,
               updated_at text)"""
 
-cursor.execute (schema)
+cursor.execute(schema)
 
-subprocess.call (['touch', 'temp.txt'])
+subprocess.call(['touch', 'temp.txt'])
 
 header = datetime.now().strftime("%B %-d, %Y") #December 7, 2025
 header += "\n" + datetime.now().strftime("%A") #Tuesday
@@ -26,10 +26,11 @@ header += "\n" + datetime.now().strftime("%I:%M:%S %p") + "\n\n" #07:09:12 PM
 
 file = open('temp.txt','w')
 
-file.write (header)
+file.write(header)
 
- #TODO : find ways to seperate the closes and capture catches more than one errors
 file.close()
-cursor.close()
 
-subprocess.call (['nano', file.name])
+subprocess.call(['nano', file.name])
+
+
+cursor.close()
