0eca02dc19254ee53f51993074dcdb68af77267a
Palaniappan V <vallippanpalam2006@gmail.com>
2026-05-16 17:46:40 +0530

    table (schema) created

--- database.py
+++ database.py
@@ -4,4 +4,12 @@ connection = sqlite3.connect ("journal.db")
 
 cursor = connection.cursor ()
 
-cursor.execute ("CREATE TABLE if not exists")
+schema = """CREATE TABLE entries (
+              journalid int primary key,
+              journal_date text,
+              journal_text text,
+              created_at text,
+              updated_at text)"""
+
+if (cursor.execute (schema)):
+  print ("Database connection successful")
