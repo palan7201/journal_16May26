4e9f0d15ff1ce8a29f6d5435affdcc401651a854
Palaniappan V <vallippanpalam2006@gmail.com>
2026-05-16 18:21:39 +0530

    subprocess: nano init

--- database.py
+++ database.py
@@ -1,15 +1,28 @@
+#journal_16May26/database.py
+
 import sqlite3
+import subprocess
 
 connection = sqlite3.connect ("journal.db")
+print ("Database connection successful")
 
 cursor = connection.cursor ()
 
-schema = """CREATE TABLE entries (
+schema = """CREATE TABLE entries IF NOT EXISTS (
               journalid int primary key,
               journal_date text,
               journal_text text,
               created_at text,
               updated_at text)"""
 
-if (cursor.execute (schema)):
-  print ("Database connection successful")
+try :
+  cursor.execute (schema)
+
+except :
+  pass
+
+finally:
+  print ("Table created successfully")
+
+
+subprocess.call(['nano', 'temp.txt'])
