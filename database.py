647fe8981e98350890747557161a850c7c528366
Palaniappan V <vallippanpalam2006@gmail.com>
2026-05-17 13:57:43 +0530

    loading text from db & structural changes

--- database.py
+++ database.py
@@ -3,34 +3,57 @@
 import sqlite3
 import subprocess
 from datetime import datetime
+import sys
+import os
+import tempfile
+import getopt
 
+#Database connection - schema creation (if not exists)
 connection = sqlite3.connect("journal.db")
-print("Database connection successful")
+print("Database connection successful", file = sys.stderr)
 
 cursor = connection.cursor()
 
 schema = """CREATE TABLE IF NOT EXISTS entries (
-              journal_id int primary key,
-              journal_date text,
+              journal_date text primary key,
               journal_text text,
               created_at text,
-              updated_at text)"""
+              updated_at text);
+         """
 
 cursor.execute(schema)
+j_file = tempfile.NamedTemporaryFile(mode = 'w', delete = False)
 
-subprocess.call(['touch', 'temp.txt'])
+# getting system (local) date & request date
+current_date = datetime.now().strftime("%d-%m-%Y")
+if (len(sys.argv)<1):
+    request_date = sys.argv[1]
+else:
+    request_date = current_date
 
-header = datetime.now().strftime("%B %-d, %Y") #December 7, 2025
-header += "\n" + datetime.now().strftime("%A") #Tuesday
-header += "\n" + datetime.now().strftime("%I:%M:%S %p") + "\n\n" #07:09:12 PM
+#Processing command-line arguments
+#find if the journal should be created or updated - database
+db_file = cursor.execute("""select journal_text from entries where journal_date = '%s';""" %request_date)
+db_file = cursor.fetchone()
 
-file = open('temp.txt','w')
+if(db_file is not None):
+    j_file.write(db_file[0])
 
-file.write(header)
+else:
+    # Writing file header & TODO: new row - database
+    j_header = datetime.now().strftime("%B %-d, %Y\n") #December 7, 2025
+    j_header += datetime.now().strftime("%A\n") #Tuesday
+    j_header += datetime.now().strftime("%I:%M:%S %p\n\n") #07:09:12 PM
+    j_file.write(j_header)
 
-file.close()
 
-subprocess.call(['nano', file.name])
+#Calling the editor for editor
+j_file.close()
+subprocess.call([os.getenv("EDITOR", "nano"), "--", j_file.name])
 
+# TODO: fetch journal file data & complete the query after thinking through
+#cursor.execute("""insert into entries values ('%s', , ,'%s')""" %request_date %)
 
+j_file.close()
 cursor.close()
+
