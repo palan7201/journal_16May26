cbd2d7e44aa34cecdcdb0783529b73bcd8f1683a
Palaniappan V <vallippanpalam2006@gmail.com>
2026-05-18 00:26:30 +0530

    refactoring: subtle

--- database.py
+++ database.py
@@ -25,11 +25,10 @@ cursor.execute(schema)
 j_file = tempfile.NamedTemporaryFile(mode = 'w', delete = False)
 
 # getting system (local) date & request date
-current_date = datetime.now().strftime("%d-%m-%Y")
+request_date = datetime.now().strftime("%d-%m-%Y")
 if (len(sys.argv)>1):
     request_date = sys.argv[1]
-else:
-    request_date = current_date
+
 print("Requested date : %s" %request_date, file = sys.stderr)
 
 #Processing command-line arguments
@@ -50,28 +49,25 @@ else:
     j_file.write(j_header)
     modified = False
 
-#Calling the editor for editor
+#Calling the editor for editing journal file
 j_file.close()
+time = datetime.now().strftime("%d-%m-%Y %I:%M:%S %p") # journal opening time
+
 subprocess.run([os.getenv("EDITOR", "nano"), "--", j_file.name])
 
-# fetch journal file data & UPSERT query
+# fetch journal file data for update and insert query
 j_file = open(j_file.name, 'r')
 journal_text = j_file.read()
 print("\nThe content read from the journal file is : \n%s" %journal_text, file = sys.stderr)
 
-# getting the journal file's opening time
-argument = j_header.split()
-created_at = ''
-updated_at = created_at = created_at.join(argument[-2:])
-
 #writing to database
 if(modified):
     update = """update entries set journal_text= ?, updated_at=? where journal_date = ?"""
-    cursor.execute(update, (journal_text, updated_at, request_date))
+    cursor.execute(update, (journal_text, time, request_date))
     print("Updated the journal", file = sys.stderr)
 else:
     insert = """insert into entries(journal_date, journal_text, created_at, updated_at) values (?, ?, ?, null)"""
-    cursor.execute(insert, (request_date, journal_text, created_at))
+    cursor.execute(insert, (request_date, journal_text, time))
     print("Inserted into the journal", file = sys.stderr)
 
 #closing the connections
